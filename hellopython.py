18dce1082fccc4bddd067a92b90e5636e42877ce
meissatouncara <meissa.touncara@uvs.edu.sn>
2021-07-15 11:21:30 +0000

    Importation de pygame

--- hellopython.py
+++ hellopython.py
@@ -3,3 +3,7 @@ print('HELLO MAISSA I AM SOULEYMANE')
 print ("Hello World")
 print("Bonjour c'est  Moustapha")
 print('HELLO ERICS')
+import pygame
+from pygame.locals import *
+pygame.init()
+fenetre = pygame.display.set_mode((640, 480))
