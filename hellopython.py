539f86fd5159494963ee1e189ced890d2ae808f3
SY <ibrahimasy275@gmail.com>
2021-07-15 15:38:53 +0000

    test sy

--- hellopython.py
+++ hellopython.py
@@ -3,7 +3,7 @@ print('HELLO MAISSA I AM SOULEYMANE')
 print ("Hello World")
 print("Bonjour c'est  Moustapha")
 print('HELLO ERICS')
-print('HELLO SY')
+print('HELLO SY2')
 import pygame
 from pygame.locals import *
 pygame.init()
