d7a8e07615b678c4185cc31a6db6775d1c99d925
meissatouncara <meissa.touncara@uvs.edu.sn>
2021-07-28 21:19:10 +0000

    chrono et cadre fin jeu

--- hellopython.py
+++ hellopython.py
@@ -5,12 +5,34 @@
 
 import pygame
 from pygame.locals import *
+from datetime import timedelta, datetime, date, time
+def _make_chrono_label(chrono):
+    "Crée une Surface représentant le temps du chrono"
+    return font.render(chrono.strftime("%H : %M : %S"),
+                       True, (255, 255, 255))
+ 
+def update(chrono, label, dt):
+    """Mise à jour du temps écoulé.
+ 
+    dt est le nombre de millisecondes
+    """
+    old_chrono = chrono
+    chrono += timedelta(milliseconds=dt)
+    # Comme le chrono n'indique pas les fractions de secondes,
+    # on ne met à jour le label que si quelque chose de visible a changé
+    if old_chrono.second != chrono.second:
+        label = _make_chrono_label(chrono)
+    return chrono, label
 
 pygame.init()
 
 ecran = pygame.display.set_mode((700, 450))
 image = pygame.image.load("maquette2.png").convert_alpha()
 serpent = pygame.image.load("serpent.jpg").convert_alpha()
+font = pygame.font.Font(None, 25)
+chrono = datetime.combine(date.today(), time(0, 0))
+label = _make_chrono_label(chrono)
+fps_clock = pygame.time.Clock()
 #ecran.blit(image, (0, 0))
 Sx=8
 Sy=6
@@ -20,6 +42,9 @@ def new_func():
 while jeu:
     ecran.blit(image, (0, 0))
     ecran.blit(serpent, (95+Sx*30, 30+Sy*30))
+    dt = fps_clock.tick(60)
+    chrono, label = update(chrono, label, dt) 
+    
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key==K_DOWN :
@@ -30,6 +55,16 @@ while jeu:
                 Sx+=1
             elif event.key== K_LEFT:
                 Sx-=1
+            if event.key == K_SPACE:
+                jeu = False
+                print("Fermer la fenetre")
+    print(Sx)
+    ecran.blit(label, (520, 10))
+    if Sx == 0 or Sx == 17:
+        jeu = False
+    elif Sy == 0 or Sy== 13:
+        jeu = False 
+    
     pygame.display.flip()
     
 
